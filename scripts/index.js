688223231e5439577d52ebbb91e59f1c33949c8a
dashi imekov <coradoburrows@yandex.ru>
2020-10-02 09:23:51 +0800

    исправления 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,29 @@
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__form');
+let popupUserName = popup.querySelector('.popup__input-text_type_username');
+let popupDescription = popup.querySelector('.popup__input-text_type_description');
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__container');
+let userName = document.querySelector('.profile__username');
+let description = document.querySelector('.profile__description');
 
-function editData(){
-	let popup = document.querySelector('.popup');
-	let userName = popup.querySelector('.popup__input-text_type_username');
-	let description = popup.querySelector('.popup__input-text_type_description');
-	
-	userName.value = document.querySelector('.profile__username').textContent;
-	description.value = document.querySelector('.profile__description').textContent; 
-	popup.classList.add('popup_opened');
+function openPopup() {
+    popupUserName.value = userName.textContent;
+    popupDescription.value = description.textContent;
+    popup.classList.add('popup_opened');
 }
 
-function formSubmitHandler(evt){
-	evt.preventDefault();
-	let popup = document.querySelector('.popup');
-	let userName = document.querySelector('.profile__username');
-	let description = document.querySelector('.profile__description');
-
-	userName.textContent= document.querySelector('.popup__input-text_type_username').value;
-	description.textContent= document.querySelector('.popup__input-text_type_description').value; 
-	popup.classList.remove('popup_opened');
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    userName.textContent = popupUserName.value;
+    description.textContent = popupDescription.value;
+    closePopup();
 }
 
-function closePopup(){
-	let popup = document.querySelector('.popup');
-	popup.classList.remove('popup_opened');	
+function closePopup() {
+    popup.classList.remove('popup_opened');
 }
 
-
-editButton.addEventListener('click', editData);
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
